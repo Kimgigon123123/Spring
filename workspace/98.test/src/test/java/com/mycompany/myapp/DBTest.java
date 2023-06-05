0b6ee983fac3ff062915719257d8fd54d209800c
Kimgigon123123 <128439525+Kimgigon123123@users.noreply.github.com>
2023-06-05 18:07:39 +0900

    6.5
    6.5

--- workspace/98.test/src/test/java/com/mycompany/myapp/DBTest.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.myapp;
-
-import org.junit.runner.RunWith;
-
-@RunWith(Spring)
-public class DBTest {
-
-}
---
+++ workspace/999.practice/src/main/java/com/my/app/MemberController.java
@@ -0,0 +1,32 @@
+package com.my.app;
+
+
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+
+
+@Controller
+public class MemberController {
+	
+	
+
+	
+	@RequestMapping("/member")
+	public String member(HttpSession session,Model model) {
+		
+		
+	
+		return "/member/member";
+	}
+	
+}	
